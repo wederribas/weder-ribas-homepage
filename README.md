a961376057a113d6f1f4f3f3d0c1d3720a68fb14
Weder Ribas <wederribas@gmail.com>
2017-09-27 22:05:20 -0300

    Create project initial structure

---
+++ README.md
@@ -0,0 +1,4 @@
+## My Personal Page
+
+Welcome! That's my personal website source code. It's under development now. So, take a coffee and come back later to see more code. ;-)
+
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var config = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
+}
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env' : {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  );
+}
+
+module.exports = config;
