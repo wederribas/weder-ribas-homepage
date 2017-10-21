f02a21b5269693acccf5d49d405eef400eec1a6c
Weder Ribas <wederribas@gmail.com>
2017-10-21 15:47:05 -0200

    Remove react-icons support replacing for fontAwesome

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,13 +1,17 @@
 const React = require('react');
 const NavLink = require('react-router-dom').NavLink;
-const GitHubIcon = require('react-icons/lib/go/mark-github');
-const TwitterIcon = require('react-icons/lib/fa/twitter');
-const LinkedinIcon = require('react-icons/lib/fa/linkedin-square');
+
+const socialMediaLink = {
+  'github': 'https://github.com/wederribas',
+  'linkedin': 'https://www.linkedin.com/in/weder-alves-ribas-87b33568',
+  'slack': 'https://ribaslabs.slack.com',
+  'twitter': 'https://twitter.com/wederribas'
+}
 
 class Nav extends React.Component {
   render() {
     return (
-      <nav className='navbar navbar-expand-lg navbar-dark bg-dark flex-column flex-md-row'>
+      <nav className='navbar navbar-expand-lg navbar-light flex-column flex-md-row' style={{'backgroundColor': 'white'}}>
         <NavLink exact className='nav-brand' to='/'>
           <img
             src={require('../images/brand.png')}
@@ -20,25 +24,46 @@ class Nav extends React.Component {
         <div className='d-flex flex-row ml-md-3'>
           <ul className='navbar-nav flex-row'>
             <li className='nav-item'>
-              <NavLink exact className='nav-link' activeClassName='active' to='/'>Home</NavLink>
+              <NavLink exact className='nav-link' activeClassName='active' to='/'>
+                <i className='fa fa-home fa-fw fa-lg' aria-hidden='true'></i>&nbsp;Home
+              </NavLink>
             </li>
             <li className='nav-item'>
-              <NavLink exact className='nav-link' activeClassName='active' to='/portfolio'>Portfolio</NavLink>
+              <NavLink exact className='nav-link' activeClassName='active' to='/portfolio'>
+                <i className='fa fa-briefcase fa-fw fa-lg' aria-hidden='true'></i>&nbsp;Portfolio
+              </NavLink>
             </li>
             <li className='nav-item'>
-              <NavLink exact className='nav-link' activeClassName='active' to='/about'>About me</NavLink>
+              <NavLink exact className='nav-link' activeClassName='active' to='/about'>
+                <i className='fa fa-user fa-fw fa-lg' aria-hidden='true'></i>&nbsp;About me
+              </NavLink>
             </li>
           </ul>
         </div>
         <ul className='navbar-nav flex-row ml-md-auto d-none d-md-flex'>
           <li className='nav-item'>
-            <a className='nav-link' target="_blank" href='https://github.com/wederribas'><GitHubIcon /></a>
+            <a className='nav-link' target='_blank' aria-hidden='true' href={socialMediaLink.github}>
+              <i className='fa fa-github fa-lg' title='My Github account'></i>
+              <span className='sr-only'>My Github account</span>
+            </a>
           </li>
           <li className='nav-item'>
-            <a className='nav-link' target="_blank" href='https://twitter.com/wederribas'><TwitterIcon /></a>
+            <a className='nav-link' target='_blank' aria-hidden='true' href={socialMediaLink.linkedin}>
+              <i className='fa fa-linkedin fa-lg' title='My Linkedin account'></i>
+              <span className='sr-only'>My Linkedin account</span>
+            </a>
           </li>
           <li className='nav-item'>
-            <a className='nav-link' target="_blank" href='https://www.linkedin.com/in/weder-alves-ribas-87b33568'><LinkedinIcon /></a>
+            <a className='nav-link' target='_blank' aria-hidden='true' href={socialMediaLink.slack}>
+              <i className='fa fa-slack fa-lg' title='My Slack workspace'></i>
+              <span className='sr-only'>My Slack workspace</span>
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a className='nav-link' target='_blank' aria-hidden='true' href={socialMediaLink.twitter}>
+              <i className='fa fa-twitter fa-lg' title='My Twitter account'></i>
+              <span className='sr-only'>My Twitter account</span>
+            </a>
           </li>
         </ul>
         <button type='button' className='btn btn-bd-yellow d-none d-lg-inline-block ml-md-3'>Hire me!</button>
